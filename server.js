9b0dd5861114f69607375623970eca437103b45c
Timurrrrrrrrrrrr <romashkin.timur13@mail.ru>
2022-03-03 09:36:04 +0300

    добавляет маршрутизацию

--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const requestHandler = (request, response) => {
-response.setHeader("Content-Type", "text/html; charset=utf-8;");
-response.write("<h2>hello world</h2>");
-response.end();
-};
-http.createServer(requestHandler).listen(3000);
