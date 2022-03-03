1146f940e423ab0a6c351852efb7fbd1aceb6629
Timurrrrrrrrrrrr <romashkin.timur13@mail.ru>
2022-03-03 17:40:36 +0300

    попытка выполнения 2го задания

--- web-server/server.js
+++ web-server/server.js
@@ -6,16 +6,12 @@ const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     if (request.url === "/home" || request.url === "/") {
         response.write("<h2>hello world</h2>");
-        fs.appendFileSync("hello.txt", 'переход в hello\n' );
+        fs.appendFileSync("hello.txt", 'переход в hello\n');
     } else if (request.url == "/about") {
         response.write("<h2>About</h2>");
-        fs.appendFileSync("hello.txt",'переход в about\n' );
-    } else if (request.url == "/contact") {
-        response.write("<h2>Contacts</h2>");
-        fs.appendFileSync("hello.txt",'переход в contact\n' );
-    } else {
-        response.write("<h2>Not found</h2>");
-        fs.appendFileSync("hello.txt",'переход в NF\n' );
+
+    } else if (request.url == "/image/1") {
+        response.end( < img src = 'https://www.example.com/images/dinosaur.jpg' > )
     }
     response.end();
 
